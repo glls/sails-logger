3427a6445762470dadbf83f4819c511322c60f5a
George Litos <gl@iti.gr>
2017-07-14 16:54:22 +0300

    Limit rows and sort by time
    Signed-off-by: George Litos <gl@iti.gr>

--- api/controllers/LogController.js
+++ api/controllers/LogController.js
@@ -6,19 +6,31 @@
  */
 
 module.exports = {
-
   view: function(req, res) {
+
+    return res.view();
+
+    /*
+    var rows = 1000;
+
     Log.find()
+      .limit(rows)
+      .sort('createdAt DESC')
       .exec(function(err, logs) {
         return res.view({
           logs: logs
         });
       });
+      */
   },
 
 
   dtjson: function(req, res) {
+    var rows = 1000;
+
     Log.find()
+      .limit(rows)
+      .sort('createdAt DESC')
       .exec(function(err, logs) {
         return res.json({
           data: logs
