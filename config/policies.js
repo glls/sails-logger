05dce0bc89ad9430ff25793ee3dfa06024f90099
George Litos <gl@iti.gr>
2017-07-14 16:56:02 +0300

    access rights and policies
    Signed-off-by: George Litos <gl@iti.gr>

--- config/policies.js
+++ config/policies.js
@@ -16,36 +16,66 @@
  * http://sailsjs.org/#!/documentation/reference/sails.config/sails.config.policies.html
  */
 
+// Authentication module.
+var auth = require('http-auth');
+
+var basic = auth.basic({
+  realm: "Mathisis Logging"
+}, (username, password, callback) => {
+  // Custom authentication.
+  // Use callback(error) if you want to throw async error.
+  callback(username === "user" && password === "password");
+});
+
 
 module.exports.policies = {
 
   /***************************************************************************
-  *                                                                          *
-  * Default policy for all controllers and actions (`true` allows public     *
-  * access)                                                                  *
-  *                                                                          *
-  ***************************************************************************/
+   *                                                                          *
+   * Default policy for all controllers and actions (`true` allows public     *
+   * access)                                                                  *
+   *                                                                          *
+   ***************************************************************************/
 
-  // '*': true,
+  '*': true,
 
   /***************************************************************************
-  *                                                                          *
-  * Here's an example of mapping some policies to run before a controller    *
-  * and its actions                                                          *
-  *                                                                          *
-  ***************************************************************************/
-	// RabbitController: {
+   *                                                                          *
+   * Here's an example of mapping some policies to run before a controller    *
+   * and its actions                                                          *
+   *                                                                          *
+   ***************************************************************************/
+  // RabbitController: {
 
-		// Apply the `false` policy as the default for all of RabbitController's actions
-		// (`false` prevents all access, which ensures that nothing bad happens to our rabbits)
-		// '*': false,
+  // Apply the `false` policy as the default for all of RabbitController's actions
+  // (`false` prevents all access, which ensures that nothing bad happens to our rabbits)
+  // '*': false,
 
-		// For the action `nurture`, apply the 'isRabbitMother' policy
-		// (this overrides `false` above)
-		// nurture	: 'isRabbitMother',
+  // For the action `nurture`, apply the 'isRabbitMother' policy
+  // (this overrides `false` above)
+  // nurture  : 'isRabbitMother',
 
-		// Apply the `isNiceToAnimals` AND `hasRabbitFood` policies
-		// before letting any users feed our rabbits
-		// feed : ['isNiceToAnimals', 'hasRabbitFood']
-	// }
+  // Apply the `isNiceToAnimals` AND `hasRabbitFood` policies
+  // before letting any users feed our rabbits
+  // feed : ['isNiceToAnimals', 'hasRabbitFood']
+  // }
+
+
+  logController: {
+    // enable http-auth policy for UI
+    // *
+    // 'create'
+    //
+    'view': auth.connect(basic),
+    'dtjson': auth.connect(basic),
+    'find': auth.connect(basic),
+    'findOne': auth.connect(basic),
+    // disable delete, update
+    'destroy': false,
+    'update': false,
+    'populate': false,
+    'add': false,
+    'remove': false,
+  }
+
 };
