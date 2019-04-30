ff54739246efe3a0c31983b49cc1c632c82c72fa
George Litos <gl@cyberpunk.gr>
2019-04-30 07:55:03 +0300

    info & formatting

--- test.py
+++ test.py
@@ -2,6 +2,9 @@ import json, requests, random, string, datetime, time
 from random import randint
 from random_words import RandomWords
 
+# install random words with pip
+# pip3 install --user randomwords
+
 url = "http://localhost:1337/log/create"
 
 action_words = ['PUT', 'GET', 'POST', 'DELETE', 'UPDATE']
@@ -14,19 +17,19 @@ headers = {'Content-type': 'application/json',
 start = time.time()
 
 for i in range(0, 100):
-    log = {
-        "message": rw.random_word('m'),
-        "session": "5950eed348d4167818c36a"+str(i),
-        "component": random.choice(component_words),
-        "method": random.choice(action_words),
-        "level": randint(1, 5),
-        "location": "/random/" + str(i),
-        "data": rw.random_word('d'),
-    }
+  log = {
+    "message": rw.random_word('m'),
+    "session": "5950eed348d4167818c36a" + str(i),
+    "component": random.choice(component_words),
+    "method": random.choice(action_words),
+    "level": randint(1, 5),
+    "location": "/random/" + str(i),
+    "data": rw.random_word('d'),
+  }
 
-    data = json.dumps(log)
-    print (data)
-    r = requests.post(url, data=data, headers=headers)
-    print (r.text)
+  data = json.dumps(log)
+  print(data)
+  r = requests.post(url, data=data, headers=headers)
+  print(r.text)
 
-print ('It took', time.time()-start, 'seconds.')
+print('It took', time.time() - start, 'seconds.')
